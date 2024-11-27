e524b03de1963cf9591ab894e5dd13252796eae1
Leonardo L. <leo@kassiopeia.co>
2024-11-28 00:59:32 +0100

    comments and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# passwdgen
+noting thoughts for the future:
+- specify amount of special characters in password, or use a random number n > 0 as amount.
+- first map random characters to full length, then iterate with special chars.
+- do smth with gaussian distribution, idk?
+- generate cryptographic secrets.
--- passwd.go
+++ passwd.go
@@ -12,18 +12,18 @@ type password struct {
 }
 
 func newPass(l int) *password {
-	return &password{
+	return &password{ // return address to new password? or return new password's address
 		length: l,
 	}
 }
 
 func (p *password) utfCharConfig() error {
-	utfUppercaseBoundary := [2]rune{'A', 'Z'}
-	var utfUppercase []rune
+	utfUppercaseBoundary := [2]rune{'A', 'Z'} // define rune boundary
+	var utfUppercase []rune                   // create empty rune array
 	for r := utfUppercaseBoundary[0]; r <= utfUppercaseBoundary[1]; r++ {
-		utfUppercase = append(utfUppercase, r)
+		utfUppercase = append(utfUppercase, r) // append every rune in the rune boundary to utfUppercase
 	}
-	p.uppercaseCharset = append(p.uppercaseCharset, utfUppercase...)
+	p.uppercaseCharset = append(p.uppercaseCharset, utfUppercase...) // probably couldve shorten this with a pointer to p.utfUppercase to save memory
 
 	utfLowercaseBoundary := [2]rune{'a', 'z'}
 	var utfLowercase []rune
@@ -39,22 +39,25 @@ func (p *password) utfCharConfig() error {
 	}
 	p.uppercaseCharset = append(p.uppercaseCharset, utfNumbers...)
 
-	utfSpecial := []rune{'!', '#', '+', '/', '&', '%', ';', '?', '-', '_'}
+	utfSpecial := []rune{'!', '#', '+', '/', '&', '%', ';', '?', '-', '_'} // this could later allow to unselect special characters
 	p.uppercaseCharset = append(p.uppercaseCharset, utfSpecial...)
 
 	return nil
 }
 
 func (p *password) genPass() error {
-	tmppass := ""
+	tmppass := "" // empty pass
+
+	// this could be modified later, not sure how you can shorten this
 	baseChars := p.uppercaseCharset
 	baseChars = append(baseChars, p.lowercoseCharset...)
 	baseChars = append(baseChars, p.numberCharset...)
 	baseChars = append(baseChars, p.specialCharset...)
 
+	// iterate p.length times thru baseChars
 	for i := 0; i < p.length; i++ {
-		randIndex := rand.IntN(len(baseChars))
-		tmppass += string(baseChars[randIndex])
+		randIndex := rand.IntN(len(baseChars))  // retrieve random int in range of how many items baseChar has
+		tmppass += string(baseChars[randIndex]) // append typecasted baseChar at random index to tmppass string
 	}
 	p.pass = tmppass
 	return nil
